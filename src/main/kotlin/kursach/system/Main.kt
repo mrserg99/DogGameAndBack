5af81dc1f459dee0e65b0552a1da1d91b81ae3d3
mrser <mrserg19@gmail.com>
2024-02-29 17:41:52 +0300

    pretty code beck

--- src/main/kotlin/kursach/system/Main.kt
+++ src/main/kotlin/kursach/system/Main.kt
@@ -1,14 +1,11 @@
 package kursach.system
 
-import kursach.system.dto.User
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -32,18 +29,19 @@ class Main
     }
 
     @PostMapping(value = ["/registration"])
-    fun registration(@RequestBody login: String, @RequestBody pass: String): ResponseEntity<String> {
+    fun registration(@RequestParam(value = "login") login: String, @RequestParam(value = "pass") pass: String?): ResponseEntity<String> {
         if(true){ //TODO: обращение к БД RegisterUser
-            return ResponseEntity("TEST", HttpStatus.OK)
+            return ResponseEntity(login, HttpStatus.OK)
         } else {
             return ResponseEntity(HttpStatus.UNAUTHORIZED)
         }
     }
 
-    @PostMapping(value = ["/field"])
-    fun field(@RequestBody status: String): ResponseEntity<HttpStatus> {
-        return ResponseEntity(HttpStatus.OK)
-    }
+//    @PostMapping(value = ["/field"])
+//    fun field(@RequestBody status: String): ResponseEntity<HttpStatus> {
+//        return ResponseEntity(HttpStatus.OK)
+//    }
+
 
 }
 
