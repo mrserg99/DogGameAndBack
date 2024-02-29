0dcada8eab19e76dcee7617cc48cc9755ed0fb72
mrser <mrserg19@gmail.com>
2024-02-29 17:54:46 +0300

    add logger

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,12 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-web") {
+		exclude("org.springframework.boot", "spring-boot-starter-logging")
+	}
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-log4j2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.exposed:exposed-core:0.47.0")
--- src/main/kotlin/kursach/system/Main.kt
+++ src/main/kotlin/kursach/system/Main.kt
@@ -1,5 +1,6 @@
 package kursach.system
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.HttpStatus
@@ -12,27 +13,38 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @SpringBootApplication
 @Controller
-class Main
-{
+class Main {
+
+    companion object{
+        val log = LoggerFactory.getLogger(Main::class.java)
+    }
+
     @RequestMapping(value = ["/"])
     fun welcome(): String {
+        log.info("Подключение к сервису")
         return "login"
     }
 
     @PostMapping(value = ["/login"])
     fun login(@RequestParam(value = "login") login: String, @RequestParam(value = "pass") pass: String?): ResponseEntity<String> {
+        log.info("Авторизация - Логин = ${login}, Пароль = $pass")
         if(true){ //TODO: обращение к БД registration_user
+            log.info("Авторизация - вернул $login")
             return ResponseEntity(login, HttpStatus.OK)
         } else {
+            log.error("Авторизация - ошибка, некорректные данные")
             return ResponseEntity(HttpStatus.UNAUTHORIZED)
         }
     }
 
     @PostMapping(value = ["/registration"])
     fun registration(@RequestParam(value = "login") login: String, @RequestParam(value = "pass") pass: String?): ResponseEntity<String> {
+        log.info("Регистрация - Логин = ${login}, Пароль = $pass")
         if(true){ //TODO: обращение к БД RegisterUser
+            log.info("Регистрация - вернул $login")
             return ResponseEntity(login, HttpStatus.OK)
         } else {
+            log.error("Регистрация - ошибка, некорректные данные")
             return ResponseEntity(HttpStatus.UNAUTHORIZED)
         }
     }
