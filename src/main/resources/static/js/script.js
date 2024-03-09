c509232e347ea2508d32c4e8dba8417dff289a27
coffinbean <evgeniazabirova07@gmail.com>
2024-03-09 13:49:54 +0300

    changes

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -26,9 +26,9 @@ var uName = uName||function log() {
 
 
 function name() {
-    document.getElementById("user_name").innerHTML =  document.cookie.slice(20, document.cookie.length)+"\u{1F43E}";
+  //  document.getElementById("user_name").innerHTML =  document.cookie.slice(20, document.cookie.length)+"\u{1F43E}";
     //document.getElementById("user_name").innerHTML =  document.cookie.split("login=")+"\u{1F43E}";
-//    document.getElementById("user_name").innerHTML =  getCookie("login=")+"\u{1F43E}";
+      document.getElementById("user_name").innerHTML =  getCookie(login)+"\u{1F43E}";
 
 }
 function uname(){
