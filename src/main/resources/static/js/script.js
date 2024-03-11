c94e5285c8b57ce322d51cd63ec0429dc3a1be25
coffinbean <evgeniazabirova07@gmail.com>
2024-03-12 02:51:39 +0300

    commit edu changes

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -54,56 +54,61 @@ function uname(){
     document.getElementById("u30_name").innerHTML =  getCookie(per);
     document.getElementById("u31_name").innerHTML =  getCookie(per);
 
+    let game_id = "game_id"
+    var gameId = JSON.parse(getCookie(field))["gameId"];
 
-     var var_json = JSON.parse(getCookie(field));
+    document.cookie =game_id +"="+ gameId
 
-    if(var_json[0].cellId===11){
-        get_cell(var_json[0].resourceId,var_json[0].countOfResources,"11_b","11_202","11_c","11_p1","11_p2","11_p3","11_p4");
-    }
-    if(var_json[1].cellId===12){
-        get_cell(var_json[1].resourceId,var_json[1].countOfResources,"12_b","12_202","12_c","12_p1","12_p2","12_p3","12_p4");
-    }
-    if(var_json[2].cellId===13){
-        get_cell(var_json[2].resourceId,var_json[2].countOfResources,"13_b","13_202","13_c","13_p1","13_p2","13_p3","13_p4");
-    }
-    if(var_json[3].cellId===14){
-        get_cell(var_json[3].resourceId,var_json[3].countOfResources,"14_b","14_202","14_c","14_p1","14_p2","14_p3","14_p4");
-    }
+    var cells = JSON.parse(getCookie(field))["cells"];
 
-    if(var_json[4].cellId===15){
-        get_cell(var_json[4].resourceId,var_json[4].countOfResources,"15_b","15_202","15_c","15_p1","15_p2","15_p3","15_p4");
-    }
 
-    if(var_json[5].cellId===16){
-        get_cell(var_json[5].resourceId,var_json[5].countOfResources,"16_b","16_202","16_c","16_p1","16_p2","16_p3","16_p4");
+    if(cells[0].cellId===11){
+        get_cell(cells[0].resourceId,cells[0].countOfResources,"11_b","11_202","11_c","11_p1","11_p2","11_p3","11_p4");
+    }
+    if(cells[1].cellId===12){
+        get_cell(cells[1].resourceId,cells[1].countOfResources,"12_b","12_202","12_c","12_p1","12_p2","12_p3","12_p4");
+    }
+    if(cells[2].cellId===13){
+        get_cell(cells[2].resourceId,cells[2].countOfResources,"13_b","13_202","13_c","13_p1","13_p2","13_p3","13_p4");
+    }
+    if(cells[3].cellId===14){
+        get_cell(cells[3].resourceId,cells[3].countOfResources,"14_b","14_202","14_c","14_p1","14_p2","14_p3","14_p4");
     }
 
-    if(var_json[6].cellId===17){
-        get_cell(var_json[6].resourceId,var_json[6].countOfResources,"17_b","17_202","17_c","17_p1","17_p2","17_p3","17_p4");
+    if(cells[4].cellId===15){
+        get_cell(cells[4].resourceId,cells[4].countOfResources,"15_b","15_202","15_c","15_p1","15_p2","15_p3","15_p4");
     }
 
-    if(var_json[7].cellId===18){
-        get_cell(var_json[7].resourceId,var_json[7].countOfResources,"18_b","18_202","18_c","18_p1","18_p2","18_p3","18_p4");
+    if(cells[5].cellId===16){
+        get_cell(cells[5].resourceId,cells[5].countOfResources,"16_b","16_202","16_c","16_p1","16_p2","16_p3","16_p4");
     }
 
-    if(var_json[8].cellId===19){
-        get_cell(var_json[8].resourceId,var_json[8].countOfResources,"19_b","19_202","19_c","19_p1","19_p2","19_p3","19_p4");
+    if(cells[6].cellId===17){
+        get_cell(cells[6].resourceId,cells[6].countOfResources,"17_b","17_202","17_c","17_p1","17_p2","17_p3","17_p4");
     }
 
-    if(var_json[9].cellId===20){
-        get_cell(var_json[9].resourceId,var_json[9].countOfResources,"1a_b","1a_202","1a_c","1a_p1","1a_p2","1a_p3","1a_p4");
+    if(cells[7].cellId===18){
+        get_cell(cells[7].resourceId,cells[7].countOfResources,"18_b","18_202","18_c","18_p1","18_p2","18_p3","18_p4");
     }
 
-    if(var_json[10].cellId===21){
-        get_cell(var_json[10].resourceId,var_json[10].countOfResources,"21_b","21_202","21_c","21_p1","21_p2","21_p3","21_p4");
+    if(cells[8].cellId===19){
+        get_cell(cells[8].resourceId,cells[8].countOfResources,"19_b","19_202","19_c","19_p1","19_p2","19_p3","19_p4");
     }
 
-    if(var_json[11].cellId===22){
-        get_cell(var_json[11].resourceId,var_json[11].countOfResources,"22_b","22_202","22_c","22_p1","22_p2","22_p3","22_p4");
+    if(cells[9].cellId===20){
+        get_cell(cells[9].resourceId,cells[9].countOfResources,"1a_b","1a_202","1a_c","1a_p1","1a_p2","1a_p3","1a_p4");
     }
 
-    if(var_json[12].cellId===23){
-        get_cell(var_json[12].resourceId,var_json[12].countOfResources,"23_b","23_202","23_c","23_p1","23_p2","23_p3","23_p4");
+    if(cells[10].cellId===21){
+        get_cell(cells[10].resourceId,cells[10].countOfResources,"21_b","21_202","21_c","21_p1","21_p2","21_p3","21_p4");
+    }
+
+    if(cells[11].cellId===22){
+        get_cell(cells[11].resourceId,cells[11].countOfResources,"22_b","22_202","22_c","22_p1","22_p2","22_p3","22_p4");
+    }
+
+    if(cells[12].cellId===23){
+        get_cell(cells[12].resourceId,cells[12].countOfResources,"23_b","23_202","23_c","23_p1","23_p2","23_p3","23_p4");
     }
     document.getElementById("wrapper_34").classList.add("dis_none")
 
