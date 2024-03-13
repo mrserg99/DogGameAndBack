40f8918850b20fa11e9b7651912eccbac4cd8a1c
coffinbean <evgeniazabirova07@gmail.com>
2024-03-13 10:49:20 +0300

    add getXmlHttp

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,22 @@
 
+function getXmlHttp() {
+    var x = false;
+    try {
+        x = new XMLHttpRequest();
+    } catch (e) {
+        try {
+            x = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (ex) {
+            try {
+                req = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e1) {
+                x = false;
+            }
+        }
+    }
+    return x;
+}
+
 /* POST-запрос */
 var uName = uName||function log() {
     var login = document.getElementById("login").value; // Считываем значение
