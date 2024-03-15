92257df816ce6f0e63776d405f3f32cf54e13891
coffinbean <evgeniazabirova07@gmail.com>
2024-03-15 15:45:29 +0300

    create Query remote

--- src/main/kotlin/kursach/system/repository/QueryRemove.kt
+++ src/main/kotlin/kursach/system/repository/QueryRemove.kt
@@ -35,7 +35,7 @@ class QueryRemove: Query {
     }
 
     override fun createGameFieldQuery(gameId: Int): List<Cell> {
-        QueryLocal.log.info("Вызов процедуры создания игрового поля")
+        log.info("Вызов процедуры создания игрового поля")
 
         val query = prepareQuery(Procedures.createGameField, gameId)
 
@@ -45,8 +45,7 @@ class QueryRemove: Query {
     }
 
     override fun getGameFieldQuery(gameId: Int): List<Cell> {
-        QueryLocal.log.info("Вызов процедуры получения созданного игрового поля")
-
+     log.info("Вызов процедуры получения созданного игрового поля")
 
         val query = prepareQuery(Procedures.getGameField, gameId)
 
@@ -56,67 +55,146 @@ class QueryRemove: Query {
     }
 
     override fun moveQuery(login: String, position: Int, gameId: Int): List<PlayerResources> {
-        TODO("Not yet implemented")
+     log.info("Вызов процедуры хода игрока")
+        val query = prepareQuery(Procedures.move, login, position, gameId)
+
+        val response = URL(query).readText()
+
+        val queryFinishMove = prepareQuery(Procedures.playerMoveFalse, login, gameId)
+
+        URL(queryFinishMove)
+
+        val querySetNextPlayerMove = prepareQuery(Procedures.setMoveNextPlayer, login, gameId)
+
+        URL(querySetNextPlayerMove)
+
+        return listOf(response as PlayerResources) // TODO:
     }
 
     override fun takeMeMoveQuery(login: String, gameId: Int) {
-        TODO("Not yet implemented")
-    }
+
+         val query = prepareQuery(Procedures.playerMoveTrue, login, gameId)
+
+         URL(query)
+
+       }
 
     override fun setMoveNextPlayerQuery(login: String, gameId: Int) {
-        TODO("Not yet implemented")
-    }
+
+            val query = prepareQuery(Procedures.setMoveNextPlayer, login, gameId)
+
+            URL(query)
+           }
 
     override fun playerFinished(login: String, gameId: Int) {
-        TODO("Not yet implemented")
-    }
+             log.info("Вызов процедуры игрок на финише")
+
+             val query = prepareQuery(Procedures.playerFinished, login, gameId)
+
+             URL(query)
+
+         }
 
     override fun everyoneFinish(gameId: Int): Boolean {
-        TODO("Not yet implemented")
-    }
+         log.info("Вызов процедуры проверки все ли финишировали")
+
+            val query = prepareQuery(Procedures.everyoneFinish, gameId)
+
+            val response = URL(query).readText()
+
+            return response.toBoolean();//TODO
+        }
 
     override fun createGameQuery(gameName: String): Int {
-        TODO("Not yet implemented")
-    }
+            log.info("Вызов процедуры создания игры")
+
+            val query = prepareQuery(Procedures.createGame, gameName)
+
+            val response = URL(query).readText()
+
+           return response.toInt();//TODO
+       }
 
     override fun createPlayerQuery(login: String, gameID: Int, dogId: String) {
-        TODO("Not yet implemented")
+           log.info("Вызов процедуры игрок на финише")
+
+           val query =  prepareQuery(Procedures.createPlayer, login, gameID, mapDog(dogId))
+
+           URL(query)
     }
 
     override fun availableGamesQuery(): List<Games> {
-        TODO("Not yet implemented")
-    }
+            log.info("Вызов процедуры всех доступных игр")
+
+            val query = prepareQuery(Procedures.availableGames)
+
+            val response = URL(query).readText()
+
+            return listOf(response as Games) // TODO:
+     }
+
 
     override fun gameStart(gameId: Int) {
-        TODO("Not yet implemented")
-    }
+            log.info("Вызов процедуры начала игры")
+
+            val query =  prepareQuery(Procedures.gameStart, gameId)
+
+            URL(query)
+     }
 
     override fun gameFinish(gameId: Int) {
-        TODO("Not yet implemented")
+            log.info("Вызов процедуры окончания игры")
+
+            val query =  prepareQuery(Procedures.gameFinish, gameId)
+
+            URL(query)
     }
 
     override fun gameStarted(gameId: Int): Boolean {
-        TODO("Not yet implemented")
-    }
+            log.info("Вызов процедуры проверки начала игры")
+
+            val query = prepareQuery(Procedures.gameStarted, gameId)
+
+            return response.toBoolean();//TODO
+     }
 
     override fun getEnemyLoginQuery(login: String, gameId: Int): String {
-        TODO("Not yet implemented")
+            log.info("Вызов процедуры получения логина соперника")
+
+            val query = prepareQuery(Procedures.getEnemyLogin, login, gameId)
+
+            return response.toString();//TODO
     }
 
     override fun setFirstMoveQuery(gameId: Int) {
-        TODO("Not yet implemented")
+               log.info("Вызов процедуры выбора игрока для первого хода")
+
+               val query = prepareQuery(Procedures.setFirstMove, gameId)
+
+               URL(query)
     }
 
     override fun gameStatusQuery(gameId: Int): Int {
-        TODO("Not yet implemented")
-    }
+            log.info("Вызов процедуры проверки статуса")
+
+            val query = prepareQuery(Procedures.gameStatus, gameId)
+
+            return response.toInt();//TODO
 
     override fun canMove(login: String, gameId: Int): Boolean {
-        TODO("Not yet implemented")
+            log.info("Вызов процедуры выбора игрока для первого хода")
+
+             val query = prepareQuery(Procedures.canPlayerMove, login, gameId)
+
+             return response.toBoolean(); // TODO:
     }
 
     override fun whereEnemyQuery(login: String, gameId: Int): Int {
-        TODO("Not yet implemented")
+              log.info("Вызов процедуры поиска позиции соперника")
+
+              val query = prepareQuery(Procedures.enemiesPlace, login, gameId)
+
+              return response.toInt();//TODO
     }
 
     private fun prepareQuery(procedure: String, vararg arguments: Any): String {
